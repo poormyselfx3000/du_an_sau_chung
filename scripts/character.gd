04f23742ccb75120d0c61194844acd0b4d8be5d5
poormyselfx3000 <vipchvaythoi3@gmail.com>
2024-09-03 21:48:21 -0700

    e da lam them cai chia khoa

--- scripts/character.gd
+++ scripts/character.gd
@@ -18,7 +18,7 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		rotate_y(deg_to_rad(-event.relative.x * mouse_sens))
 		head.rotate_x(deg_to_rad(-event.relative.y * mouse_sens))
-		head.rotation.x = clamp(head.rotation.x,deg_to_rad(-40),deg_to_rad(89))
+		head.rotation.x = clamp(head.rotation.x,deg_to_rad(-70),deg_to_rad(89))
 
 
 func _physics_process(delta):
