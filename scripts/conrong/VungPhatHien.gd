b92bbbe7304cd1aee0aa63ce74178a220df4a9b4
Phu Dinh <phudinhtruongk18@gmail.com>
2024-09-05 07:42:36 +0700

    go go go

--- scripts/conrong/VungPhatHien.gd
+++ scripts/conrong/VungPhatHien.gd
@@ -1,13 +1,18 @@
 class_name VungPhatHien extends Area3D
 
 var player_in_the_area: CharacterBody3D = null
+@onready var conrong = $".."
 
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		print("Player Da Den")
 		player_in_the_area = body
+		player_in_the_area.text_set("oh shit I fuk up")
+		conrong.tucgian()
 
 func _on_body_exited(body):
 	if body.is_in_group("player"):
 		print("Player Run Away")
+		player_in_the_area.text_set("yay")
 		player_in_the_area = null
+		conrong.vuive()
--- scripts/conrong/conrong.gd
+++ scripts/conrong/conrong.gd
@@ -4,37 +4,46 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+var target: Vector3 = Vector3(0,0,0)
+
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func set_target_position(new_position: Vector3):
-	pass
+	target = new_position
+
+
+#nav
+@onready var nav: NavigationAgent3D = $NavigationAgent3D
+	
 
 
 #------- SLIME view
+
 @onready var vung_phat_hien = $VungPhatHien
+
+func tucgian():
+	look_at(vung_phat_hien.player_in_the_area.position)
+	
+func vuive():
+	pass
+
 
 func player_in_vung_phat_hien() -> CharacterBody3D:
 	return vung_phat_hien.player_in_the_area
 #------- SLIME view
 
+const ACCEL = 10
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	var direction = Vector3()
+	nav.target_position = target
+	direction = nav.get_next_path_position() - global_position
+	direction = direction.normalized()
+	
+	velocity = velocity.lerp(direction * SPEED, ACCEL * delta)
 
 	move_and_slide()
