b97d6fd6bbef99bb27131e36b7715d62079a207e
poormyselfx3000 <vipchvaythoi3@gmail.com>
2024-09-04 21:14:55 -0700

    de them mo cua dong cua va vai thu khac

--- scripts/character.gd
+++ scripts/character.gd
@@ -3,8 +3,11 @@ extends CharacterBody3D
 @onready var head = $truccodinh
 @onready var cam_animation = $cam_animation
 @onready var hand_animation = $hand_animation
+@onready var warning = $warning
+@onready var timer = $Timer
 
-const SPEED = 3.5
+var show_text = ""
+const SPEED = 3
 const JUMP_VELOCITY = 4.5
 var SMOOTH_FACTOR = 0.3
 @export var mouse_sens = 0.1
@@ -34,32 +37,25 @@ func _physics_process(delta):
 	
 	if Input.is_action_just_pressed("interactive") and nearkey == true:
 		check_key = true
-	
+		
 	if Input.is_action_just_pressed("interactive") and neardoor == true and check_key == true and $"../door".open == false:
 		$"../door".animation.play("open")
 		$"../door".open = true
+		text_set("mở được rồi này")
+	elif Input.is_action_just_pressed("interactive") and neardoor == true and check_key == false and $"../door".open == false:
+		text_set("cửa đã khóa, mình cần tìm chìa khóa để mở.")
+
 		
 	var input_dir = Input.get_vector("a", "d", "w", "s")
 	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-		if is_on_floor():
-			cam_animation.play("camshake")
-			hand_animation.play("hand_shake")
-		else:
-			cam_animation.play("jump")
-			hand_animation.play("hand_jump")
+		trang_thai_di_chuyen()
 	else:
 		velocity.x = lerp(float(velocity.x), 0.0, SMOOTH_FACTOR)
 		velocity.z = lerp(float(velocity.z), 0.0, SMOOTH_FACTOR)
-		if is_on_floor():
-			cam_animation.play("RESET")
-			hand_animation.play("RESET")
-		
-		else:
-			cam_animation.play("jump")
-			hand_animation.play("hand_jump")
+		trang_thai_dung_yen()
 	move_and_slide()
 
 
@@ -67,6 +63,28 @@ func _on_key_body_entered(body):
 	var target = body
 	if target.is_in_group("player"):
 		nearkey = true
+		text_set("Ồ một chiếc chìa khóa")
+		
+func text_set(set_text):
+	warning.clear()
+	warning.add_text(set_text)
+	timer.start()
+
+func trang_thai_di_chuyen():
+	if is_on_floor():
+		cam_animation.play("camshake")
+		hand_animation.play("hand_shake")
+	else:
+		cam_animation.play("jump")
+		hand_animation.play("hand_jump")
+
+func  trang_thai_dung_yen():
+	if is_on_floor():
+		cam_animation.play("RESET")
+		hand_animation.play("RESET")
+	else:
+		cam_animation.play("jump")
+		hand_animation.play("hand_jump")
 
 func _on_key_body_exited(body):
 	var target = body
@@ -84,3 +102,7 @@ func _on_door_body_exited(body):
 	var target = body
 	if target.is_in_group("player"):
 		neardoor = false
+
+
+func _on_timer_timeout():
+	text_set("...")
